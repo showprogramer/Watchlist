8a2ff0ae2b5e58ae18cb4ec238d8c2670d9e1e3b
Gumayusi <w1647114628@gmail.com>
2025-12-30 19:24:52 +0800

    添加一个简单的首页

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
 from markupsafe import escape
 from flask import url_for
+import os
 
+# 获取上级目录的 fronted 文件夹
+template_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'fronted')
+app = Flask(__name__, template_folder=template_dir)
 
-app = Flask(__name__)
+print(os.path.dirname(os.path.dirname(__file__)))
+
+#模拟数据
+name = 'Grey Li'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
+
+@app.route('/index')
+def index():
+    return render_template('index.html', name=name, movies=movies)
 
 @app.route('/')
 def hello():
